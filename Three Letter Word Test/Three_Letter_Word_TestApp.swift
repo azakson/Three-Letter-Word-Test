9aebcb66a4006bdd6cd14dbaa9e62e151c00b569
Avery <Zakson>
2021-10-25 10:20:47 -0500

    Initial Commit

---
+++ Three Letter Word Test/Three_Letter_Word_TestApp.swift
@@ -0,0 +1,17 @@
+//
+//  Three_Letter_Word_TestApp.swift
+//  Three Letter Word Test
+//
+//  Created by Student on 10/25/21.
+//
+
+import SwiftUI
+
+@main
+struct Three_Letter_Word_TestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
